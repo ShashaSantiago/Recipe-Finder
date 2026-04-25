6aa9c64c0d66ec808f1095d7e2510b36e76fa62e
Sharmaine Santiago <sharmainejoysantiago183@gmail.com>
2026-04-25 10:38:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+
+/* =================
+// MEAL API FROM THEMEALDB
+=================*/
+
+let recipes = [];
+let currentPage = 1;
+const itemsPerPage = 6;
+
+var baseUrl = "https://www.themealdb.com/api/json/v1/1/search.php"
+document.addEventListener("DOMContentLoaded", ()=>{
+    loadSuggestedRecipes()
+})
+
+function loadSuggestedRecipes()
+{
+        fetch(`${baseUrl}?s=chicken`)
+        .then(res => res.json())
+        .then(data => {
+            recipes = data.meals || []
+            console.log(recipes);
+})
+}
+function displayRecipes()
+{
+    const container = document.getElementById("recipeContainer")
+    container.innerHTML = " "
+
+    const start = (currentPage -1) * itemsPerPage;
+    const paginated = recipes.slice(start, start + itemsPerPage)
+
+    paginated.forEach(meal => {
+        const col = document.createElement("div")
+        col.className = "col-md mb-3 fade-in"
+        
+        let ingredients = "";
+        for (let i =1; i <= 8; i++)
+        {
+            let ing = meal[`strIngredients${i}`];
+            if(ing && ing.trim !== ""){
+                ingredients += `<li> ${ing} </li>`
+            }
+        }
+
+        col.innerHTML = `
+        <div class="flip-card" onclick="flipCard(this, event)">
+            <div class="flip-card-inner">
+            
+            <!-- FRONT -->
+            <div class="flip-card-front card">
+                <img src="${meal.strMealThumb}" class="card-img-top">
+                <div class`
+    })
+}
